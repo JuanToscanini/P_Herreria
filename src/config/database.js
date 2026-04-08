63c2532759604a17e9c8a5d9b717a63a8ffb1dd5
Robert <rober.villarrubiaa@gmail.com>
2026-04-08 20:39:39 -0300

    conexion con base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://admin:program4@ac-wcxiiy0-shard-00-00.zjh0t3g.mongodb.net:27017,ac-wcxiiy0-shard-00-01.zjh0t3g.mongodb.net:27017,ac-wcxiiy0-shard-00-02.zjh0t3g.mongodb.net:27017/tp_zapatillas?ssl=true&replicaSet=atlas-gsiay7-shard-0&authSource=admin&appName=Cluster0');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB conectado correctamente');
   } catch (error) {
     console.error('Error al conectar MongoDB:', error);
